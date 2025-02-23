064b1a5b883de10a457b3309c18b9f808cf61175
Shifat507 <ferdoushasanshifat@gmail.com>
2025-02-23 15:11:57 +0600

    update backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -28,7 +28,35 @@ async function run() {
 
         
         app.get('/tasks', async(req, res)=>{
+           
             const result = await tasksCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/latestTask', async(req, res)=>{
+           
+            const result = await tasksCollection
+            .find()
+            .sort({ date: -1, time: -1 }) // Sort by date and time in descending order
+            .limit(4)
+            .toArray();
+            res.send(result);
+        })
+        app.get('/toDo', async(req, res)=>{
+           
+            const query = {status : "To Do"}
+            const result = await tasksCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.get('/inProgress', async(req, res)=>{
+           
+            const query = {status : "In Progress"}
+            const result = await tasksCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.get('/taskDone', async(req, res)=>{
+           
+            const query = {status : "Done"}
+            const result = await tasksCollection.find(query).toArray();
             res.send(result);
         })
         // add a new task
